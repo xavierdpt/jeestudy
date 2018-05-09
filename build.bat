ddfdffaa49e930dcd9f0b6ad3f98eb5e1a7b490d
xavierdpt <xavierdpt>
2018-05-09 16:15:26 +0530

    .

--- build.bat
+++ build.bat
@@ -1,2 +1,5 @@
 type top.html main.html bottom.html >index.html
 type top.html jeestudy1\main.html bottom.html >jeestudy1\index.html
+type top.html jeestudy2\main.html bottom.html >jeestudy2\index.html
+type top.html jeestudy3\main.html bottom.html >jeestudy3\index.html
+type top.html jeestudy4\main.html bottom.html >jeestudy4\index.html
