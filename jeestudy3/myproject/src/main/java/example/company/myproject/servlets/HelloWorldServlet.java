43a34117ceea597f2fe36ecb1182923c0b4ac2ee
xavierdpt <xavierdpt>
2018-05-09 21:39:46 +0530

    .

---
+++ jeestudy3/myproject/src/main/java/example/company/myproject/servlets/HelloWorldServlet.java
@@ -0,0 +1,24 @@
+package example.company.myproject.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.annotation.Generated;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/helloworld")
+public class HelloWorldServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+		writer.println(Generated.class.getName());
+
+	}
+}
